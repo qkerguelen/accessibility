e03540d6d0255e18022f368ae1cc6aa76213aea4
Quentin Kerguélen <q.kerguelen@outlook.com>
2015-10-08 15:54:20 +0200

    cleaning bitch

--- site/index.php
+++ site/index.php
@@ -4,11 +4,11 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width"/>
         <title>Title</title>
-        <link rel="icon" type="image/png" href="img/triangle.png">
+        <link rel="icon" type="image/png" href="">
         <link rel="stylesheet" media="screen" href="css/reset.css">
         <link rel="stylesheet" type="text/css" href="css/style.css">
-        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-        <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+        <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+        <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
         <script src="js/main.js"></script>
     </head>
     <body>
