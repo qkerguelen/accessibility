b8eb1d9b034485f6d82692ce684a9ad6d7f25d37
Quentin Kerguélen <q.kerguelen@outlook.com>
2015-10-08 16:43:30 +0200

    correction amour

--- site/index.php
+++ site/index.php
@@ -1,26 +1,6 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width"/>
-        <title>Title</title>
-        <link rel="icon" type="image/png" href="img/triangle.png">
-        <link rel="stylesheet" media="screen" href="css/reset.css">
-        <link rel="stylesheet" type="text/css" href="css/style.css">
-        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-        <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-        <script src="js/main.js"></script>
-    </head>
-    <body>
-        <?php 
-        	include("header.php");
-        	include("footer.php");
-        ?>
-    </body>
-</html>
-<!DOCTYPE html>
-<html>
-    <head>
         <html lang="fr">
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width"/>
