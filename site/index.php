2f5351eaa00f5bd95a6023ec1fff60eb4b1dde6b
“ZussyFranck” <zussy.franck@gmail.com>
2015-11-04 14:55:58 +0100

    cssfranck

--- site/index.php
+++ site/index.php
@@ -8,6 +8,7 @@
         <link rel="icon" type="image/png" href="">
         <link rel="stylesheet" media="screen" href="css/reset.css">
         <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="stylesheet" type="text/css" href="css/style-franck.css">
         <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
         <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
         <script src="js/main.js"></script>
